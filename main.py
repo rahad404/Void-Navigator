1a2dbea6f8b9358b4b06b91d232fb924c5a734b8
rahad404 <rahad.0911@gmail.com>
2026-06-13 11:07:58 +0600

    add: button class

--- main.py
+++ main.py
@@ -28,3 +28,46 @@ COLOR_ACCENT_RED = (255, 60, 60)     # Asteroids, hazardous targets, alarms
 COLOR_ACCENT_AMBER = (255, 180, 0)   # Hazard warning borders
 COLOR_CLOSED_LIST = (255, 60, 60, 45) # A* Search space overlays (with alpha)
 COLOR_OPEN_LIST = (0, 255, 120, 45)   # A* Search space overlays (with alpha)
+
+# ui helper classes
+
+# button class
+class Button:
+    def __init__(self, rect, text, action_id, active_color=COLOR_ACCENT_CYAN, toggle_state=None):
+        self.rect = pygame.Rect(rect)
+        self.text = text
+        self.action_id = action_id
+        self.active_color = active_color
+        self.is_hovered = False
+        self.toggle_state = toggle_state  # toggle switch (True/False)
+
+    def draw(self, surface, font):
+        # Determine background and border colors based on state
+        if self.toggle_state is not None:
+            border_color = self.active_color if self.toggle_state else COLOR_HUD_BORDER
+            bg_color = (self.active_color[0]//5, self.active_color[1]//5, self.active_color[2]//5) if self.toggle_state else (12, 18, 30)
+        else:
+            border_color = self.active_color if self.is_hovered else COLOR_HUD_BORDER
+            bg_color = (self.active_color[0]//8, self.active_color[1]//8, self.active_color[2]//8) if self.is_hovered else (12, 18, 30)
+
+        # Draw button box
+        pygame.draw.rect(surface, bg_color, self.rect)
+        pygame.draw.rect(surface, border_color, self.rect, 1)
+
+        # Draw decorative corner ticks for the sci-fi look
+        if self.is_hovered or (self.toggle_state is True):
+            tick_len = 5
+            pygame.draw.line(surface, self.active_color, (self.rect.left, self.rect.top), (self.rect.left + tick_len, self.rect.top), 2)
+            pygame.draw.line(surface, self.active_color, (self.rect.left, self.rect.top), (self.rect.left, self.rect.top + tick_len), 2)
+            pygame.draw.line(surface, self.active_color, (self.rect.right - 1, self.rect.bottom - 1), (self.rect.right - 1 - tick_len, self.rect.bottom - 1), 2)
+            pygame.draw.line(surface, self.active_color, (self.rect.right - 1, self.rect.bottom - 1), (self.rect.right - 1, self.rect.bottom - 1 - tick_len), 2)
+
+        # Draw text
+        text_color = COLOR_TEXT_BRIGHT if (self.is_hovered or self.toggle_state) else COLOR_TEXT_MUTED
+        txt_surface = font.render(self.text, True, text_color)
+        txt_rect = txt_surface.get_rect(center=self.rect.center)
+        surface.blit(txt_surface, txt_rect)
+
+    def check_hover(self, mouse_pos):
+        self.is_hovered = self.rect.collidepoint(mouse_pos)
+        return self.is_hovered
